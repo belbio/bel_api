0a4230c7010752d167cb05c08addec097737604d
William Hayes <william.s.hayes@gmail.com>
2017-06-29 15:34:57 -0400

    Updated install.sh script

--- bin/install.sh
+++ bin/install.sh
@@ -6,36 +6,32 @@ hash docker 2>/dev/null || { echo >&2 "I require docker. Please install.  Aborti
 hash docker-compose 2>/dev/null || { echo >&2 "I require docker-compose. Please install.  Aborting."; exit 1; }
 
 
-ssh_status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 git@bitbucket.com 2>&1)
+ssh_status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 git@github.com 2>&1)
 
 if [[ $ssh_status == *"successfully authenticated"* ]] ; then
-  clone_cmd="git clone git@bitbucket.org:pmiworks/bel_api.git";
+  clone_cmd="git clone git@github.com:belbio/bel_api.git";
 else
-  clone_cmd="git clone https://wshayes@bitbucket.org/pmiworks/bel_api.git";
+  clone_cmd="git clone https://github.com/belbio/bel_api.git";
 fi
 
 if [ ! -d "bel_api" ]; then
     $clone_cmd
-else
-    cd bel_api;
-    git pull;
-    cd ..;
-fi
 
 cd bel_api
 HOME=$(pwd)
 
-if [ ! -f "$HOME/config.yml" ]; then
-    cp config.yml.example config.yml
+git pull;
+
+if [ ! -f "$HOME/api/config.yml" ]; then
+    cp $HOME/api/config.yml.example $HOME/api/config.yml
+    printf "Remember to update api/config.yml\n\n"
 fi
 
-printf "Remember to update config.yml\n\n"
+echo "Starting to build the docker containers"
+docker-compose build
+docker-compose create
+
 
 echo "To start the docker containers: "
-echo "  docker-compose build"
-echo "  docker-compose up"
-
-printf "\n\nTo run production docker: "
-echo "  docker-compose -f docker-compose-prod.yml build"
-echo "  docker-compose -f docker-compose-prod.yml up"
+echo "  docker-compose start or docker-compose up"
 
