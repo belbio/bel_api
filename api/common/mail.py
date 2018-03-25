5ba4c3d1937fa6ec8fcd14e2c0be64bd26f39b6e
William Hayes <william.s.hayes@gmail.com>
2018-03-25 09:33:30 -0400

    Add ability to send mail via mailgun
    Needs to be re-factored to be more mail service agnostic - pull requests welcome

---
+++ api/common/mail.py
@@ -0,0 +1,18 @@
+import requests
+
+from bel.Config import config
+
+
+def send_mail(mail_to: str, subject: str, msg: str, mail_from: str = config['bel_api']['mail']['admin_email']):
+    """Send mail using MailGun API"""
+
+    data = {
+        "to": mail_to,
+        "from": mail_from,
+        "subject": subject,
+        "text": msg,
+    }
+
+    request = requests.post(config['bel_api']['mail']['api'], auth=('api', config['bel_api']['mail']['api_key']), data=data)
+    return request
+
